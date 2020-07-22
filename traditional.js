ec103e19cacdd8fdea90a3f45a4e4e0916482884
pritampanhale <31956760+pritampanhale@users.noreply.github.com>
2020-07-22 20:11:47 +0530

    Create traditional.js

---
+++ traditional.js
@@ -0,0 +1,9 @@
+method1().then((res)=>{
+    method2().then((res2)=>{
+        method3().then((res3)=>{
+            method4().then((res4)=>{
+                // final check
+            })
+        })
+    })
+})
